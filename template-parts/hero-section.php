a46643fd3d45a2a25246460a6f4e73d342d4a467
Fred <huertaserick@yahoo.ie>
2026-02-16 16:08:50 +0000

    Hero content added

--- template-parts/hero-section.php
+++ template-parts/hero-section.php
@@ -10,8 +10,9 @@
         <h1>Hey, I'm Fred 👋</h1>
         <p>Junior Web Developer turning ideas into clean, intuitive websites. I specialize in WordPress, PHP dashboards, and custom tools that make life easier for users. Curious, organized, and always building something new.</p>
         <a href="<?php echo get_template_directory_uri(); ?>/assets/pdfs/FredCV2026.pdf"
-            class="btn"
+            class="btn-cv"
             target="_blank"
-            download>Download CV</a>
+            download>Download CV
+        </a>
     </div>
 </section>
