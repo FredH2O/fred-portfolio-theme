5dc74947b3e817448f17408db3c099d61419f581
Fred <huertaserick@yahoo.ie>
2026-03-19 18:45:46 +0000

    Added project listings, project description and image output

--- archive-project.php
+++ archive-project.php
@@ -13,7 +13,56 @@
     </div>
 
     <hr />
-    <div>
+
+    <!-- display projects here -->
+    <div class="project-list">
+
+        <?php if (have_posts()) : ?>
+            <?php while (have_posts()) : the_post(); ?>
+
+                <div class="project">
+                    <!-- thumbnail image -->
+                    <?php if (has_post_thumbnail()) : ?>
+                        <div class="project-thumbnail">
+                            <?php the_post_thumbnail('full'); ?>
+                        </div>
+                    <?php endif; ?>
+
+                    <!-- short description -->
+                    <?php
+                    $short_description = get_field('short_description');
+                    if ($short_description) : ?>
+                        <p><?php echo esc_html($short_description); ?></p>
+                    <?php else: ?>
+                        <p>No description available</p>
+                    <?php endif; ?>
+
+                    <!-- image gallery -->
+                    <!-- <div class="project-gallery">
+
+                        <?php
+                        $image_1 = get_field('gallery_image_1');
+                        $image_2 = get_field('gallery_image_2');
+                        $image_3 = get_field('gallery_image_3');
+
+                        // image array
+                        $images = [$image_1, $image_2, $image_3];
+
+                        foreach ($images as $image) {
+                            if ($image) {
+                                echo wp_get_attachment_image($image['ID']);
+                            }
+                        }
+                        ?>
+
+                    </div> -->
+                </div>
+            <?php endwhile; ?>
+        <?php else : ?>
+            <p>No projects found.</p>
+        <?php endif; ?>
+
+
 
     </div>
 </section>
