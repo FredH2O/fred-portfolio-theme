b8029d95df79c9561a2fb7de32030ae8d3a3fad0
Fred <huertaserick@yahoo.ie>
2026-04-01 20:15:38 +0100

    Featured project structure update

--- single-project.php
+++ single-project.php
@@ -1,5 +1,29 @@
 <?php get_header() ?>
-<section style="height: 100vh; border: 1px solid red;">
-    <h1>This is the project's main post.</h1>
+<section class="single-project-main-container">
+
+    <?php if (get_the_title()): ?>
+        <h1><?php the_title(); ?></h1>
+    <?php endif; ?>
+
+    <?php if (get_the_content()) : ?>
+        <p><?php the_content(); ?></p>
+    <?php endif; ?>
+
+    <?php
+    $images_array = [
+        get_field('gallery_image_1'),
+        get_field('gallery_image_2'),
+        get_field('gallery_image_3'),
+    ];
+
+    if ($images_array) :
+        foreach ($images_array as $image) :
+    ?>
+
+            <img
+                src="<?php echo esc_url($image['url']); ?>"
+                alt="<?php echo esc_attr($image['alt']); ?>">
+    <?php endforeach;
+    endif; ?>
 </section>
 <?php get_footer(); ?>
