9e97dddae5c2038ea856e767b3d22a8ff55b4038
Fred <huertaserick@yahoo.ie>
2026-04-15 19:53:44 +0100

    Contact form, send a message

---
+++ page-contact.php
@@ -0,0 +1,10 @@
+<?php get_header(); ?>
+<section class="contact-form-main-container">
+
+    <h1>Contact <span>Me</span></h1>
+
+    <main class="wpform-contact-me">
+        <?php the_content(); ?>
+    </main>
+</section>
+<?php get_footer(); ?>
